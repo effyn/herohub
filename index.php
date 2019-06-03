109ca6da49c45394bd1ddd4db71086309c0c0b8f
Alicia Buehner <alicia.buehner@gmail.com>
2019-06-03 07:00:25 -0400

    Add route comments

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //Name: Alicia Buehner, Evan Wheeler
-//Date: TODO: fill in due date
+//Date: TODO: fill in due dates - Project due June 10th @ 10:00am
 //Description: This file contains the index page for...
 
 //Turn on error reporting
@@ -9,18 +9,16 @@ error_reporting(E_ALL);
 
 //Require vendor/autoload file
 require_once('vendor/autoload.php');
-//TODO: Require validation function files here
-
-//Create an instance of the Base class (instantiate Fat-Free)
-$f3 = Base::instance();
+require_once('model/validation-functions.php');
 
 // Start session
 session_start();
 
+//Create an instance of the Base class (instantiate Fat-Free)
+$f3 = Base::instance();
+
 //Turn on Fat-Free error reporting
 $f3->set('DEBUG', 3);
-
-//TODO: define additional routes below...
 
 //Define a default route to homepage
 $f3->route('GET /', function() {
@@ -28,37 +26,68 @@ $f3->route('GET /', function() {
     echo $view->render('views/homepage.html');
 });
 
-//Define route for the first form page user registration information
+//Define route to the first form page user registration information
 $f3->route('GET|POST /register', function() {
+
+    //get data from form - $variable = $_POST['']
+
+    //add data to the hive - $f3->set('', $variable)
+
+    //if valid add to session (valid form) set session to variable
+        //redirect to preferences page
+
     $view = new Template();
     echo $view->render('views/register.html');
 });
 
-//Define route for second form page user play style settings
+//Define route to second form page user play style settings
 $f3->route('GET|POST /preferences', function() {
+
+    //get data from form - $variable = $_POST['']
+
+    //add data to the hive - $f3->set('', $variable)
+
+    //if valid add to session (valid form) set session to variable
+        //redirect to heroes page
+
     $view = new Template();
     echo $view->render('views/preferences.html');
 });
 
-//Define route for the third form page user player preferences
+//Define route to the third form page user player preferences
 $f3->route('GET|POST /heroes', function() {
+
+    //get data from form -  $variable = $_POST['']
+
+    //add data to the hive - $f3->set('', $variable)
+
+    //if valid add to session (valid form) set session to variable
+        //redirect to summary page
     $view = new Template();
     echo $view->render('views/heroes.html');
 });
 
-//Define route for the registration summary page
+//Define route to the registration summary page
 $f3->route('GET|POST /summary', function() {
     $view = new Template();
     echo $view->render('views/summary.html');
 });
 
-//Define route for the user login page
+//Define route to the user login page
 $f3->route('GET|POST /login', function() {
+
+    //get data from form -  $variable = $_POST['']
+
+    //add data to the hive - $f3->set('', $variable)
+
+    //if valid add to session (valid login form) set session to variable
+    //redirect to dashboard page
+
     $view = new Template();
     echo $view->render('views/login.html');
 });
 
-//Define route for the user dashboard page
+//Define route to the user dashboard page
 $f3->route('GET|POST /dashboard', function() {
     $view = new Template();
     echo $view->render('views/dashboard.html');
