d7d38b0cce06f0be5457421462df961533681a4a
Evan Wheeler <effyn@gmx.com>
2019-06-08 18:38:39 -0400

    Table definitions

---
+++ model/tables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `herohub-user` (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    platform VARCHAR(8) NOT NULL,
+    email VARCHAR(254) NOT NULL,
+    passhash VARCHAR(256) NOT NULL,
+    tag VARCHAR(64) NOT NULL,
+    region VARCHAR(16),
+    micpref int NOT NULL,
+    leaderpref int NOT NULL
+);
+
+CREATE TABLE `herohub-premiumuser` (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    role int NOT NULL,
+    hero1 VARCHAR(32),
+    hero2 VARCHAR(32),
+    hero3 VARCHAR(32),
+    FOREIGN KEY (id) REFERENCES `herohub-user` (id)
+);
